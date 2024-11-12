9aa60d8f0530fbac9d3ca168c882aae60a78a455
Jonas Reitan <7762186+Snikur@users.noreply.github.com>
2024-11-12 23:22:14 +0100

    ladder test, fixed climbing

--- components/player/climbing.gd
+++ components/player/climbing.gd
@@ -8,16 +8,18 @@ class_name ClimbingState
 var climbing_object: Node3D = null
 var attach_speed: float = 8.0
 var target_angle : float = 0.0
+var climb_speed: float = 4.0
 
 func _ready():
 	state.state_physics_processing.connect(state_physics_process)
 
 func state_physics_process(delta: float) -> void:
 	if (is_instance_valid(climbing_object)):
+		parent.transition_to(parent.ANIMATION_STATE.IDLE)
 		target_angle = climbing_object.global_rotation.y
 		parent.global_position.x = move_toward(parent.global_position.x, climbing_object.global_position.x, delta * attach_speed)
 		parent.global_position.z = move_toward(parent.global_position.z, climbing_object.global_position.z, delta * attach_speed)
-	parent.velocity = Vector3(0.0, 10.0 if Input.is_action_pressed("up") else -10.0 if Input.is_action_pressed("down") else 0.0, 0.0)
+	parent.velocity = Vector3(0.0, climb_speed if Input.is_action_pressed("up") else -climb_speed if Input.is_action_pressed("down") else 0.0, 0.0)
 	if Input.is_action_just_pressed("jump"):
 		parent.state_chart.send_event("to_ground")
 	
