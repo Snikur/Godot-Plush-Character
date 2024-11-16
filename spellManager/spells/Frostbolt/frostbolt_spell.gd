6b0a90b06e6854b3cfba0cc9d485b90bb22377bd
Jonas Reitan <7762186+Snikur@users.noreply.github.com>
2024-11-16 14:33:31 +0100

    fixed spells being cast without target

--- spellManager/spells/Frostbolt/frostbolt_spell.gd
+++ spellManager/spells/Frostbolt/frostbolt_spell.gd
@@ -8,9 +8,10 @@ func _init(spell_button: SpellTextureButton):
 	
 @rpc("authority", "call_local", "reliable")
 func cast_spell(owner: SpellManager):
-	super.cast_spell(owner)
-	var frostbolt = preload("res://spellManager/spells/Frostbolt/frostbolt.tscn").instantiate()
-	frostbolt.top_level = true
-	frostbolt.target = owner.target
-	owner.add_child(frostbolt)
-	frostbolt.global_position = owner.global_position + Vector3(0.0, 0.5, 0.0)
+	if (is_instance_valid(owner.target)):
+		super.cast_spell(owner)
+		var frostbolt = preload("res://spellManager/spells/Frostbolt/frostbolt.tscn").instantiate()
+		frostbolt.top_level = true
+		frostbolt.target = owner.target
+		owner.add_child(frostbolt)
+		frostbolt.global_position = owner.global_position + Vector3(0.0, 0.5, 0.0)
