1c283797686343869c858668d855e8f565c50dda
Jonas Reitan <7762186+Snikur@users.noreply.github.com>
2024-11-17 18:39:34 +0100

    fixed damage detection

--- spellManager/spells/Fireball/fireball.gd
+++ spellManager/spells/Fireball/fireball.gd
@@ -1,14 +1,21 @@
-extends Node3D
+extends Area3D
 
 var tween: Tween
 var target: Enemy
 @onready var vfx = $vfx_fireball
+var ticks: int = 0
 
 func _ready():
 	if (multiplayer.is_server()):
 		MM.tick.connect(tick)
+		body_entered.connect(func(body: Node3D):
+			if (body is Enemy):
+				body.request_change.rpc(-10)
+				delete.rpc()
+		)
 	else:
 		set_physics_process(false)
+		$CollisionShape3D.disabled = true
 
 func _physics_process(delta: float) -> void:
 	global_position = global_position + global_basis.z * delta * 10.0
@@ -16,6 +23,13 @@ func _physics_process(delta: float) -> void:
 
 func tick():
 	server_state.rpc(global_position)
+	ticks = ticks + 1
+	if (ticks > 50):
+		delete.rpc()
+
+@rpc("authority", "call_local", "reliable")
+func delete():
+	queue_free()
 
 @rpc("authority", "call_remote", "unreliable_ordered")
 func server_state(new_position: Vector3):
