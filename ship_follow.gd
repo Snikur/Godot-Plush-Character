05e2c328ee52f3b0b9199e7223984a42ee7ee664
Jonas Reitan <7762186+Snikur@users.noreply.github.com>
2024-11-17 22:30:12 +0100

    BOAT sync over network

--- ship_follow.gd
+++ ship_follow.gd
@@ -1,8 +1,23 @@
 extends PathFollow3D
+var tween: Tween
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "progress_ratio", 0.8, 60.0).from(0.0)
-	tween.tween_property(self, "progress_ratio", 1.0, 60.0).from(0.8).set_delay(10.0)
-	tween.set_loops(0)
+	await MM.connected
+	if (multiplayer.is_server()):
+		MM.tick.connect(tick)
+		tween = create_tween()
+		tween.tween_property(self, "progress_ratio", 0.8, 60.0).from(0.0)
+		tween.tween_property(self, "progress_ratio", 1.0, 60.0).from(0.8).set_delay(10.0)
+		tween.set_loops(0)
+	else:
+		print("client")
+
+func tick():
+	server_state.rpc(progress_ratio)
+
+@rpc("authority", "call_remote", "unreliable_ordered")
+func server_state(progress_ratio: float):
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "progress_ratio", progress_ratio, 0.1).from_current()
