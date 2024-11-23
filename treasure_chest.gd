7c5e4d60020d0ca781d1c57cfd8b2a7b51704e89
Jonas Reitan <7762186+Snikur@users.noreply.github.com>
2024-11-23 13:35:20 +0100

    Update treasure_chest.gd

--- treasure_chest.gd
+++ treasure_chest.gd
@@ -2,17 +2,20 @@ extends Area3D
 
 @onready var anim: AnimationPlayer = $chest/AnimationPlayer	
 @onready var particles: GPUParticles3D = $GPUParticles3D
-var is_open: bool = false
+@export var is_open: bool = false
 func _ready() -> void:
-	particles.emitting = is_open
+	play_animation()
 	body_entered.connect(func(body: Node3D):
 		if (body is Player and body.id == multiplayer.get_unique_id()):
-			if (anim.is_playing()):
-				return
-			is_open = not is_open
-			if (is_open):
-				anim.play("Armature|ArmatureAction")
-			else:
-				anim.play_backwards("Armature|ArmatureAction")
-			particles.emitting = is_open
+			play_animation()
 	)
+
+func play_animation():
+	if (anim.is_playing()):
+		return
+	is_open = not is_open
+	if (is_open):
+		anim.play("Armature|ArmatureAction")
+	else:
+		anim.play_backwards("Armature|ArmatureAction")
+	particles.emitting = is_open
