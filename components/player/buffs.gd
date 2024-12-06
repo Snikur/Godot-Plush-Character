5540184a3346b35a152b108be72519e2229b5b59
Jonas Reitan <snaik.tg@gmail.com>
2024-12-06 09:28:32 +0100

    yo

--- components/player/buffs.gd
+++ components/player/buffs.gd
@@ -7,9 +7,9 @@ extends Node3D
 @onready var duration5: TextureProgressBar = $CanvasLayer/PanelContainer/MarginContainer/Stack/Debuffs/Slot4/Duration
 
 func _process(_delta: float) -> void:
-	duration0.value = (duration0.value + 0.1) if duration0.value < 100 else 0
-	duration1.value = duration1.value + 0.1 if duration1.value < 100 else 0
-	duration2.value = duration2.value + 0.1 if duration2.value < 100 else 0
-	duration3.value = duration3.value + 0.1 if duration3.value < 100 else 0
-	duration4.value = duration4.value + 0.1 if duration4.value < 100 else 0
-	duration5.value = duration5.value + 0.1 if duration5.value < 100 else 0
+	duration0.value = duration0.value + 0.1 if duration0.value < 100 else 0.0
+	duration1.value = duration1.value + 0.1 if duration1.value < 100 else 0.0
+	duration2.value = duration2.value + 0.1 if duration2.value < 100 else 0.0
+	duration3.value = duration3.value + 0.1 if duration3.value < 100 else 0.0
+	duration4.value = duration4.value + 0.1 if duration4.value < 100 else 0.0
+	duration5.value = duration5.value + 0.1 if duration5.value < 100 else 0.0
