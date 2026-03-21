4edc9a112a75e085b54013f0ef5a159cf1f9d1b7
Snikur <snaik.tg@gmail.com>
2026-03-21 12:21:39 +0100

    Revert "Refactor: Add spell registry pattern and class_name for Global"
    This reverts commit 4aab86dc18bae9c38cc410597c9a29df96a0321b.

--- spellManager/spell_manager.gd
+++ spellManager/spell_manager.gd
@@ -6,13 +6,6 @@ var source: Player = null
 
 var spell_index: int = -1
 
-const SPELL_REGISTRY: Dictionary = {
-	SpellResource.SPELLTYPE.PROJECTILE: {
-		SpellResource.PROJECTILE_TYPE.FIREBALL: preload("res://spellManager/spells/Projectile/fireball_projectile_resource.tres"),
-		SpellResource.PROJECTILE_TYPE.FROSTBOLT: preload("res://spellManager/spells/Projectile/frostbolt_projectile_resource.tres")
-	}
-}
-
 @onready var action_bar : HBoxContainer = $UI/ActionBar
 
 func add_spell(new_spell: SpellResource):
@@ -21,16 +14,21 @@ func add_spell(new_spell: SpellResource):
 @rpc("any_peer", "call_remote", "reliable")
 func send_spell(type: int, projectile_type: int , index: int):
 	print(multiplayer.get_remote_sender_id(), " want to cast type ", type, " projectile ", projectile_type)
-	var casted_spell: SpellResource = get_spell(type, projectile_type)
+	var casted_spell: SpellResource = null
+	match(type):
+		SpellResource.SPELLTYPE.PROJECTILE:
+			match(projectile_type):
+				SpellResource.PROJECTILE_TYPE.FIREBALL:
+					casted_spell = preload("res://spellManager/spells/Projectile/fireball_projectile_resource.tres")
+				SpellResource.PROJECTILE_TYPE.FROSTBOLT:
+					casted_spell = preload("res://spellManager/spells/Projectile/frostbolt_projectile_resource.tres")
+				_:
+					pass
+		_:
+			pass
 	if (casted_spell):
 		casted_spell.init(null)
 		casted_spell.cast_spell(self, index)
-
-func get_spell(type: int, projectile_type: int) -> SpellResource:
-	var type_registry = SPELL_REGISTRY.get(type)
-	if type_registry:
-		return type_registry.get(projectile_type)
-	return null
 
 func get_next_spell_index() -> int:
 	spell_index = spell_index + 1
